c7dc760a099cbedc843cc0828a64ea19e42d3ad6
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-05 19:46:18 +0330

    Server source init

---
+++ server.c
@@ -0,0 +1,15 @@
+/* 
+
+Using sockets to connect to our server.
+
+Our server which will accept clients and give response to them.
+Our main input command are: 
+    1. Start [name]
+    2. Ping [server] | [user] 
+    3. Stop
+Server responses:
+    1. Init user
+    2. Pong [status]
+    3. Release client
+
+*/
