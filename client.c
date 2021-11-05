057d22f006d14eebcfe33999f71c4c93da8854fc
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-05 19:47:53 +0330

    Client source init

---
+++ client.c
@@ -0,0 +1,11 @@
+/*
+
+Client is a simple user interface to get the data
+and send it to the server by socket.
+
+Every client can do the following commands:
+    1. Start
+    2. Ping [server] | [user]
+    3. Stop
+
+*/
